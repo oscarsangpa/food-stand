5df0dbee75179c167a71d2ec8cf6e2dec085612c
oscarsangpa <oscarsangrador@gmail.com>
2023-02-17 13:09:04 +0100

    feat: create endpoint to get all orders

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -3,6 +3,16 @@ import { PrismaClient } from "@prisma/client"
 export default async function handler(req, res) {
     const prisma = new PrismaClient()
 
+    // get orders
+    const orders = await prisma.orderFood.findMany({
+        where: {
+            state: false
+        }
+    })
+    res.status(200).json(orders)
+    
+
+    // create order
     if (req.method === "POST") {
         const order = await prisma.orderFood.create({
             data: {
@@ -12,6 +22,6 @@ export default async function handler(req, res) {
                 order: req.body.order
             }
         })
-        res.json(order)
+        res.status(200).json(order)
     }
 }
